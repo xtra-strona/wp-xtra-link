5d74aadaf42e1bf71d6050cf76321bbca487bf7c
Rafał Olszewicz <wptutoriale@gmail.com>
2017-07-29 16:40:16 +0200

    Add Content Page

--- page.php
+++ page.php
@@ -1,38 +1,51 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Xtra-Link
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<br>
+        <h2>
+            <?php the_title();?>
+        </h2>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<!-- Page Content -->
+<div class="container">
 
-			<?php
-			while ( have_posts() ) : the_post();
+    <div class="row">
+        
+<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
 
-				get_template_part( 'template-parts/content', 'page' );
+        <?php else : ?>
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+          <div class="col-md-12">
 
-			endwhile; // End of the loop.
-			?>
+<?php endif ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<!-- Loop Page -->
+<?php while ( have_posts() ) : the_post();
 
-<?php
-get_sidebar();
-get_footer();
+    get_template_part('template-parts/content','page'); ?>
+<!-- Pager -->
+<div class="pager">
+     <?php previous_post_link('<span class="prev-link">%link</span>', __('&larr; Previos','xtra-link')); ?>
+     <?php next_post_link('<span class="next-link">%link</span>', __('Next &rarr;','xtra-link')); ?>
+</div>
+
+<?php 	// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif; ?>
+
+  <?php endwhile; ?>
+
+        </div>
+
+    <?php if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
+        <!-- Blog Sidebar Widgets Column -->
+        <div class="col-md-4">
+                 <?php get_sidebar(); ?>
+        </div>
+    <?php } ?>
+
+    </div><!-- /.row -->
+ </div><!-- /.container -->
+<?php get_footer(); ?>
